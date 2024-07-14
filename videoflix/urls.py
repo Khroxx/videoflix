7800a3896d28fefdba8bd935182e3d565adebc1b
Bari <134767175+Khroxx@users.noreply.github.com>
2024-07-15 00:38:37 +0200

    Revert
    This reverts commit e2f46325648fe1ad42cd54571fdca2d33ca1a2f7.

--- videoflix/urls.py
+++ videoflix/urls.py
@@ -1,22 +1,16 @@
-"""
-URL configuration for videoflix project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.shortcuts import redirect
+from content.views import VideoView
+
+def redirect_to_admin(request):
+    return redirect('/admin/')
 
 urlpatterns = [
+    # path('', redirect_to_admin),
     path('admin/', admin.site.urls),
-]
+    path('videos/', VideoView.as_view())
+
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
