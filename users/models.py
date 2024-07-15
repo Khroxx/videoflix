e24bab1210836821ddbb684b9dfa7f94e301e818
Bari <134767175+Khroxx@users.noreply.github.com>
2024-07-15 20:05:24 +0200

    register and login test + user serializer

--- users/models.py
+++ users/models.py
@@ -8,3 +8,4 @@ class CustomUser(AbstractUser):
     custom = models.CharField(max_length=1000, null=True, blank=True)
     phone = models.CharField(max_length=20, null=True, blank=True)
     address = models.CharField(max_length=150, null=True, blank=True)
+    
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.urls import reverse
+from videoflix.users.models import CustomUser
+# from django.contrib.auth import get_user_model
 
 # Create your tests here.
+
+
+class UserAccountTests(TestCase):
+    # register user
+    def test_registration(self):
+        response = self.client.post(reverse('register'), data={
+            'email': 'test@test.de',
+            'username': 'testuser',
+            'password': 'testpass123'
+        })
+        self.assertTrue(CustomUser.objects.filter(email='test@test.de').exists())
+        
+    
+    # register and login
+    def lest_login(self):
+        response = self.client.post(reverse('register'), data={
+            'email': 'test@test.de',
+            'username': 'testuser',
+            'password': 'testpass123'
+        })
+        loggedIn = self.client.post(reverse('login'), data={
+            'email': 'test@test.de',
+            'password': 'testpass123'
+        })
+        
