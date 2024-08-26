6f828e58f960fc213d001c0b6d946a2159bdcee0
Bari <134767175+Khroxx@users.noreply.github.com>
2024-08-27 00:17:11 +0200

    Update serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -3,23 +3,12 @@ from .models import CustomUser
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
-    # password = serializers.CharField(write_only=True)
-    
     class Meta:
         model = CustomUser
         fields = ('id', 'email', 'password', 'is_active', 'is_superuser', 'phone', 'custom')
         extra_kwargs = {'password': {'write_only': True}}
         
     def create(self, validated_data):
-        """
-        Create and return a new CustomUser instance, given the validated data.
-
-        Args:
-            validated_data (dict): The validated data containing user information.
-
-        Returns:
-            CustomUser: The created CustomUser instance.
-        """
         user = CustomUser.objects.create_user(
             email=validated_data['email'],
             username=validated_data['email'].split('@')[0],
@@ -30,16 +19,6 @@ class CustomUserSerializer(serializers.ModelSerializer):
         return user
     
     def update(self, instance, validated_data):
-        """
-        Update and return an existing CustomUser instance, given the validated data.
-
-        Args:
-            instance (CustomUser): The existing CustomUser instance to update.
-            validated_data (dict): The validated data containing updated user information.
-
-        Returns:
-            CustomUser: The updated CustomUser instance.
-        """
         password = validated_data.pop('password', None)
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
@@ -48,8 +27,6 @@ class CustomUserSerializer(serializers.ModelSerializer):
         instance.save()
         return instance
     
+    
 class ResetEmailSerializer(serializers.Serializer):
-    """
-    Serializer for resetting email.
-    """
     email = serializers.EmailField()
